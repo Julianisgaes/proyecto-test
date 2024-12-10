2ad830433ec2be8b126ab9a7733bb9fe620b56be
YugerNautet <Putas01@pollitas.com>
2024-12-10 14:38:45 -0700

    1.0 - Alfredo intento 3

--- views/site/contact.php
+++ views/site/contact.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        Aún no hay nada por acá.
+        Pronto abra algo.
     </p>
 
     <img src="/yii/web/media/under_construction.gif" alt="Bajo construcción">
