451e9cd1fb4916b2ec0132cade41abee0462bb2e
YugerNautet <Putas01@pollitas.com>
2024-12-12 14:45:04 -0700

    1.0 - Correcion de acerca de

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,13 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        Hola.
+        La heladeria es una empresa que nos apasiona crear y vender helados, no solo vendemos helados tambien creamos
+        momentos en familia, amigos. Nuestra empresa se ha dedicado a la elaboracion de helados artesanales de la mas
+        alta calidad.
+    </p><br/>
+    <p>
+        Cada sabor que ofrecemos se los mejores sabores que una heladeria podrai ofrecer. Tenemos los sabores calsicos
+        asta sabores creados por nosotros
     </p>
 
     <img src="<?= Yii::getAlias('@web') . '/media/under_construction.gif' ?>" alt="Bajo construcción">
