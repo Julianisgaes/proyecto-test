7273e335ecec2d13427dab28f27796878e081712
juligar <julianisgaes@gmail.com>
2024-12-13 12:29:33 -0700

    1.0 - index

--- views/site/index.php
+++ views/site/index.php
@@ -6,69 +6,30 @@ $this->title = 'La heladería';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
+    <div class="jumbotron text-center bg-transparent mb-3">
         <h1 class="display-4">¡Bienvenido!</h1>
-<br/>
-        <div class="jumbotron text-center bg-transparent">
-        <b><FONT SIZE=7>Creadores</h1></b></FONT>
-
-        <br/>
-<div class="container">
-    <div class="row">
-    <div class="col-6">
-        <FONT SIZE=6><p align="lefth">Julian Israel</p></FONT>
-        <img
-        src="https://static.wikia.nocookie.net/fa8816a5-33f5-4e32-987a-d3d1e1e8f147"
-        width="350px"
-        height="350px"
-        border="10px"
-        align="left"
-        >
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <p align="lefth">Ingresa cualquier texto</p></FONT></p>
-        </p>
-    </div>
-    <div class="col-6">
-        <FONT SIZE=6><p align="Right">Alfredo Lopez</p></FONT>
-        <img
-        src="https://stickerly.pstatic.net/sticker_pack/xO3zSKpBX8TqGJtXWsgzg/TPI8JR/16/200a8b20-a11d-4cf9-8cc7-6f12681fa5cb.png"
-        width="350px"
-        height="350px"
-        border="10px"
-        align="right"
-        >
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <p align="Right">Alfredo Lopez</p></FONT></p>
-    </div>
+        <h2>Creadores</h2>
     </div>
 
-        <img src="<?= Yii::getAlias('@web') . '/media/under_construction.gif' ?>" alt="Bajo construcción">
+    <div class="row g-5 text-center">
+        <div class="col">
+            <div class="card">
+                <img src="<?= Yii::getAlias('@web') . '/media/julian.jpg' ?>" class="card-img-top px-3 py-3" alt="julián">
+                <div class="card-body">
+                    <h5 class="card-title">Julián García</h5>
+                    <p class="card-text">Agrega algo</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="<?= Yii::getAlias('@web') . '/media/alfredo.jpg' ?>" class="card-img-top px-3 py-3" alt="julián">
+                <div class="card-body">
+                    <h5 class="card-title">Alfredo López</h5>
+                    <p class="card-text">Agrega algo</p>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
