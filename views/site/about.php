d677d7209325cc19f63eae1f91a6225fddfb46fd
YugerNautet <Putas01@pollitas.com>
2024-12-13 11:00:59 -0700

    1.0 - about

--- views/site/about.php
+++ views/site/about.php
@@ -22,3 +22,30 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <img src="<?= Yii::getAlias('@web') . '/media/acerca.jpg' ?>" alt="Helados" class="img-fluid img-thumbnail">
 </div>
+<br/>
+<div class="container">
+    <div class="row">
+    <div class="col-6">
+        <FONT SIZE=6><p align="center">Julian Israel</p></FONT>
+        <img
+        src="https://i.pinimg.com/736x/db/56/31/db56314c67691d80c63e6ecfe4ad2b39.jpg"
+        width="350px"
+        height="350px"
+        border="10px"
+        align="left"
+        >
+        <p align="center">
+        </p>
+    </div>
+    <div class="col-6">
+        <FONT SIZE=6><p align="center">Alfredo Lopez</p></FONT>
+        <img
+        src="https://i.pinimg.com/736x/e2/1a/13/e21a1323d9b4d0e5d4f0e277b1a051ae.jpg"
+        width="350px"
+        height="350px"
+        border="10px"
+        align="right"
+        >
+    </div>
+    </div>
+<br/>
